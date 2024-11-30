c1029513bf0978bad0f5e4f2b350a9f08aa7648c
Jan Poonthong <janpoonthong@Jans-MacBook-Pro.local>
2024-11-30 09:55:39 +0700

    feat: center horizontally and vertically

--- Week-4/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Week-4/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -49,16 +55,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var text by remember { mutableStateOf(currentText) }
-    TextField(
-        value = text,
-        onValueChange = { newValue ->
-            if (newValue.all { it.isDigit() } && newValue.length <= 7) {
-                text = newValue
-                currentText = newValue
-            }
-        },
-        placeholder = {Text("Student Code")}
-    )
+    Column (modifier = modifier.fillMaxSize()
+        .wrapContentWidth(Alignment.CenterHorizontally)
+        .wrapContentHeight(Alignment.CenterVertically)) {
+        TextField(
+            value = text,
+            onValueChange = { newValue ->
+                if (newValue.all { it.isDigit() } && newValue.length <= 7) {
+                    text = newValue
+                    currentText = newValue
+                }
+            },
+            placeholder = {Text("Student Code")},
+            label = {Text("Student Code")}
+        )
+    }
 }
 
 @Preview(showBackground = true)
